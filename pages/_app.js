266bc89575c26ccc4791825499ca1dbb42a56ff7
udbhav3101 <udbhav3101@gmail.com>
2022-06-14 12:35:22 +0530

    Added Homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import Layout from '../components/Layouts/main';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
+
+function MyApp({ Component, pageProps, router }) {
+
+  return(
+  <ChakraProvider> 
+  <Layout router={router}>
+    <Component {...pageProps}/>
+ </Layout> 
+  </ChakraProvider> 
+    ) 
 }
 
 export default MyApp
