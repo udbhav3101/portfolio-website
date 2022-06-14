6c720a62abcd6871a47e77504fa1e77f05ab8c06
udbhav3101 <udbhav3101@gmail.com>
2022-06-14 18:10:29 +0530

    Added Navbar

--- components/Layouts/main.js
+++ components/Layouts/main.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import {Box, Container} from '@chakra-ui/react';
-
+import Navbar from '../navbar.js';
 
 const Main = ({children, router}) =>{
     return (
@@ -9,7 +9,7 @@ const Main = ({children, router}) =>{
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <title>Udbhav - Homepage</title>
             </Head>
-
+            <Navbar path={router.asPath}/>
             <Container maxW="container.md" pt={14}>
                 {children}
             </Container>
