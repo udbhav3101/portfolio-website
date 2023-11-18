cb48223a06042cede3b7c71304c0ddf176f88d3b
udbhav3101 <udbhav3101@gmail.com>
2023-11-18 19:07:44 +0530

    changed the resume

--- pages/resume.js
+++ pages/resume.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 const Resume = () => {
   const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [pageNumber, _] = useState(1);
 
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
